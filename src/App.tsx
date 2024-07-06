91838c54d5fb2a879a0f522c8c3ea2351cde211c
John Daniel Del Monte <delmontejohndaniel80@gmail.com>
2024-07-06 15:10:17 +0800

    installed router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-import { Button } from "./components/ui/button"
+import { Outlet } from 'react-router';
 
 export default function App() {
-  return <Button>Hello World</Button>
+  return (
+    <>
+      <header>
+        <p>This is a header!</p>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './global.css';
+
+import { RouterProvider } from 'react-router-dom';
+
+import router from './router/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
